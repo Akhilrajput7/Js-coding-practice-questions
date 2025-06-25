c6ad31ee5537df884b9828a66709a2c77b1056eb
Akhil Rajput <149920374+Akhilrajput7@users.noreply.github.com>
2025-06-25 16:29:25 +0000

    Write a program that takes a number and checks whether it is positive, negative, or zero.

---
+++ Exercise-1/six.js
@@ -0,0 +1,14 @@
+// Write a program that takes a number and checks whether it is positive, negative, or zero.
+
+function checkNumber (num){
+
+   if(num > 0){
+    console.log("positive");  
+   }else if(num < 0 ){
+         console.log("negative");  
+   }else{
+    console.log("Zero");   
+   }
+}
+
+console.log(checkNumber(-1));
