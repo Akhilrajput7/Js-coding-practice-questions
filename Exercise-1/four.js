c0216709c1dece1763b3cbe3c2193124375ad7d5
Akhil Rajput <149920374+Akhilrajput7@users.noreply.github.com>
2025-06-25 16:07:43 +0000

    Write a function that takes two numbers as arguments and returns the larger number.

---
+++ Exercise-1/four.js
@@ -0,0 +1,12 @@
+// Write a function that takes two numbers as arguments and returns the larger number.
+
+function largestNumber(num1 , num2){
+     if(num1 > num2){
+        return num1
+     }
+     else{
+        return num2
+     }
+}
+
+console.log(largestNumber(9,10));
